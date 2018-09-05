a277288cf9df539ab7f99c3acdbf50cabebdc02d
Hunter Jenkins <hunterbjenkins@gmail.com>
2018-09-05 01:17:54 -0600

    no C is stupid

--- matchlab.c
+++ matchlab.c
@@ -239,7 +239,6 @@ void flag_b(char* arguments[])
 
           colonCounter++;
         }
-        //update sequence X TODO: verify that this works?
         else if (*currentCharacater >= 48 && *currentCharacater <= 57 && sequence == 0) //This means it's a digit
         {
           //Attempting to stop a seg fault before it happens.
@@ -393,23 +392,247 @@ void flag_b(char* arguments[])
        else
        {
          printf("yes\n");
-
-         // printf("stats:\n");
-         // printf("letterBRepCounter %d\n", letterBRepCounter);
-         // printf("underscoreCounter %d\n", underscoreCounter);
-         // printf("letterTCounter %d\n", letterTCounter);
-         // printf("characterEqualsCounter %d\n", characterEqualsCounter);
-         // printf("upperCaseCount %d\n", upperCaseCount);
        }
      }
    }
   }
-   printf("Flag B: \n");
 }
 
 void flag_c(char* arguments[])
 {
-   printf("Flag C: \n");
+
+
+  //This will be used to track if we are on sequence x or sequence y.
+  int sequence = 0;
+
+  //Counters
+  int letterDCounter         = 0;
+  int equalsCounter          = 0;
+
+  char sequenceX[3]          = {0};  //3 because it can't be any bigger than this!
+  int sequenceXCounter       = 0;
+
+  int letterUCounter         = 0;
+
+  int underscoreCounter      = 0;
+  char sequenceY[3]          = {0}; //2 because it can't be any bigger than this! 9
+  int sequenceYCounter       = 0;
+
+  int upperCaseCount         = 0;
+
+  char newSequenceX[3]       = {0};
+
+  int should_continue        = 1; //1 = true, 0 = false
+
+
+  //Handle each argument individually
+  int index;
+  for (index = 0; index < sizeof(arguments); index++)
+  {
+
+
+    //stop if there are no more arguments
+    if (arguments[index] == "empty") { break; }
+
+    char* currentArgument = arguments[index];
+
+    char* currentCharacater; // first copy the pointer to not change the original
+
+   //check if first character is not b
+   if (*currentArgument != 'd')
+   {
+     printf("no\n");
+     printf("initial check\n" );
+   }
+   else
+   {
+     //iterate through each character in order to check the Criteria.
+     //Update: as we are iterating, we will be checking the previous character. This
+     //will allow us to maintain the order of events.
+     for (currentCharacater = currentArgument; *currentCharacater != '\0'; currentCharacater++)
+     {
+        //Update the letterBCounter
+        if (*currentCharacater == 'd')
+        {
+          letterDCounter++;
+        }
+        //Update the colon character
+        else if (*currentCharacater == '=')
+        {
+
+          //we know that b should be done at this point. Check it's truthfulness.
+          if (letterDCounter%2 != 0) //If odd
+          {
+            printf("no\n");
+            printf("1\n");
+            should_continue = 0;
+            break;
+          }
+
+          equalsCounter++;
+        }
+        else if (*currentCharacater >= 48 && *currentCharacater <= 57 && sequence == 0) //This means it's a digit
+        {
+          //Attempting to stop a seg fault before it happens.
+          if (sequenceXCounter >=3)
+          {
+            printf("no\n");
+            printf("too many digits. Trying to stop seg fault\n");
+            should_continue = 0;
+            break;
+          }
+
+          //We know that the colon character should be done. Check it's truthfulness.
+          if (equalsCounter != 1)
+          {
+            printf("no\n");
+            printf("2\n");
+            should_continue = 0;
+            break;
+          }
+
+          printf("Adding this to sequenceX: %d\n", *currentCharacater);
+          sequenceX[sequenceXCounter] = *currentCharacater;
+          sequenceXCounter++;
+
+        }
+        else if (*currentCharacater == 'u')
+        {
+          sequence = 1;
+
+          if (sequenceXCounter > 3)
+          {
+            printf("no\n");
+            printf("3\n");
+            should_continue = 0;
+            break;
+          }
+          else if (sequenceXCounter < 1)
+          {
+            printf("no\n");
+            printf("4\n");
+            should_continue = 0;
+            break;
+          }
+
+          letterUCounter++;
+        }
+        else if (*currentCharacater == '_')
+        {
+
+          //check equals
+          if (letterUCounter == 0)
+          {
+            printf("no\n");
+            printf("6\n");
+            should_continue = 0;
+            break;
+          }
+
+
+          underscoreCounter++;
+        }
+        else if (*currentCharacater >= 48 && *currentCharacater <= 57 && sequence == 1) //checking sequence y
+        {
+
+          //Attempting to stop a seg fault before it happens.
+          if (sequenceYCounter >= 2)
+          {
+            printf("no\n");
+            printf("too many digits. Trying to stop seg fault for 'y'\n");
+            should_continue = 0;
+            break;
+          }
+
+          if (underscoreCounter != 1)
+          {
+            printf("no\n");
+            printf("8\n");
+            should_continue = 0;
+            break;
+          }
+
+          printf("Adding this to sequenceY: %d\n", *currentCharacater);
+          sequenceY[sequenceYCounter] = *currentCharacater;
+          sequenceYCounter++;
+        }
+        else if (*currentCharacater >= 65 && *currentCharacater <= 90) //uppercase
+        {
+            //now we can check the sequences
+            //iterate through sequence x, but only checking odds
+
+
+            if (sequenceX[0] != '\0')
+            {
+              newSequenceX[0] = sequenceX[0];
+            }
+            if (sequenceX[1] != '\0')
+            {
+              newSequenceX[1] = sequenceX[1];
+            }
+            if (sequenceX[2] != '\0')
+            {
+              newSequenceX[2] = sequenceX[2];
+            }
+
+
+
+            //then, we compare
+            if (newSequenceX[0] != sequenceY[2]) //if they're not a match
+            {
+              printf("no\n");
+              printf("9\n");
+              should_continue = 0;
+              break;
+            }
+            if (newSequenceX[1] != sequenceY[1]) //if they're not a match
+            {
+              printf("no\n");
+              printf("10\n");
+
+              should_continue = 0;
+              break;
+            }
+            if (newSequenceX[2] != sequenceY[1]) //if they're not a match
+            {
+              printf("no\n");
+              printf("10\n");
+
+              should_continue = 0;
+              break;
+            }
+
+            upperCaseCount++;
+        }
+        else
+        {
+          //this means a bad character was in the string
+          printf("no\n");
+          printf("current character %d\n", *currentCharacater);
+
+          // printf("Bad character%s\n", *currentCharacater);
+          should_continue = 0;
+          break;
+        }
+     }
+
+
+     if (should_continue != 0)
+     {
+       if (upperCaseCount%2 == 0) //If even
+       {
+         printf("no\n");
+         printf("7\n");
+       }
+       //Since it has passed all of these tests, we know it's matching so we
+       //print yes!
+       else
+       {
+         printf("yes\n");
+       }
+     }
+   }
+  }
 }
 
 void flag_t(char* name)
