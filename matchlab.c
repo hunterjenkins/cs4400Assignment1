3f00810a16a49ed71b6686c30674bcf7178dd030
Hunter Jenkins <hunterbjenkins@gmail.com>
2018-09-04 23:00:22 -0600

    no C is stupid

--- matchlab.c
+++ matchlab.c
@@ -331,7 +331,8 @@ void flag_b(char* arguments[])
             //then, we compare
             if (strcmp(newSequenceX,sequenceY) != 0) //if they're not a match
             {
-              printf("X:%s\n Y:%s\n",newSequenceX,  sequenceY);
+              printf("X:%s\n",newSequenceX);
+              printf("Y:%s\n" sequenceY);
               printf("no\n");
               printf("9\n");
               should_continue = 0;
