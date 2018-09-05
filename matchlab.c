af03adb8589c02cd2c9f5da3b13facd89cc21d48
Hunter Jenkins <hunterbjenkins@gmail.com>
2018-09-04 23:21:36 -0600

    no C is stupid

--- matchlab.c
+++ matchlab.c
@@ -319,8 +319,9 @@ void flag_b(char* arguments[])
 
             char* newSequenceX = "";
 
+            //TODO: FIX THIS!!!!!! How to create a new string with only odd indecies?
             int counter = 0;
-            for (currentDigit = sequenceX; *currentDigit != '\0'; currentDigit++)
+            for (currentDigit = &sequenceX; *currentDigit != '\0'; currentDigit++)
             {
               if (counter % 2 == 0) //only touch the even ones
               {
