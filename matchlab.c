ca04adb09d3fbbe6a5013129c335ec3bfaa037c3
Hunter Jenkins <hunterbjenkins@gmail.com>
2018-09-04 22:11:26 -0600

    t

--- matchlab.c
+++ matchlab.c
@@ -5,9 +5,9 @@ void flag_a(char* arguments[])
 {
    /*
    -a Criteria:                                                       STATUS
-   - any odd number of repetitions of the letter “b”;             --> TODO: find out if this is for upper and lowercase
+   - any odd number of repetitions of the letter “b”;             --> DONE
    - exactly one “_”;                                             --> DONE
-   - between 3 and 7 repetitions (inclusive) of the letter “t”;   --> TODO: find out if this is for upper and lowercase
+   - between 3 and 7 repetitions (inclusive) of the letter “t”;   --> DONE
    - exactly two “=”s; and                                        --> DONE
    - an odd number of uppercase letters.                          --> DONE
    */
@@ -34,8 +34,6 @@ void flag_a(char* arguments[])
      char* currentArgument = arguments[index];
 
      char* currentCharacater; // first copy the pointer to not change the original
-
-    //TODO: assure only one "no" is printed. I think it'll be a flag that is set/checked.
 
     //check if first character is not b
     if (*currentArgument != 'b')
@@ -158,70 +156,221 @@ void flag_a(char* arguments[])
         }
       }
     }
-
-
-
-
-
-     //Note: the if statements are more simple as to avoid bugs.
-     // I learned this technique from my internship.
-
-     // //Letter B conditions
-     // if (letterBRepCounter%2 == 0) //If even
-     // {
-     //   printf("no\n");
-     //   printf("1\n");
-     // }
-
-     //Underscore conditions
-     // else if (underscoreCounter == 0)
-     // {
-     //   printf("no\n");
-     //   printf("2\n");
-     // }
-     // else if (underscoreCounter > 1)
-     // {
-     //    printf("no\n");
-     //    printf("3\n");
-     // }
-
-     //Letter T conditions
-     // else if (letterTCounter > 7)
-     // {
-     //   printf("no\n");
-     //   printf("4\n");
-     // }
-     // else if (letterTCounter < 3)
-     // {
-     //   printf("no\n");
-     //   printf("5\n");
-     // }
-
-     //= character conditions
-     // else if (characterEqualsCounter != 2)
-     // {
-     //   printf("no\n");
-     //   printf("6\n");
-     // }
-
-     //Uppercase conditions
-     // else if (upperCaseCount%2 == 0) //If even
-     // {
-     //   printf("no\n");
-     //   printf("7\n");
-     // }
-
-
-
-
-
-
    }
 
 }
 
 void flag_b(char* arguments[])
 {
+/*
+-b Criteria:
+  -any even number (including zero) repetitions of the letter “b”;
+  -exactly two “:”s;
+  -between 1 and 3 (inclusive) decimal digits — call this sequence X;
+  -between 3 and 7 repetitions (inclusive) of the letter “q”;
+  -exactly one “=”;
+  -the same characters as the even-positioned characters in X; and
+  -an odd number of uppercase letters.
+*/
+
+//This will be used to track if we are on sequence x or sequence y.
+int sequence = 0;
+
+//Counters
+int letterBCounter         = 0;
+int colonCounter           = 0;
+char* sequenceX            = "";
+int sequenceXCounter       = 0;
+int letterQCounter         = 0;
+int equalsCounter          = 0;
+char* sequenceY            = "";
+int upperCaseCount         = 0;
+
+
+int should_continue        = 1; //1 = true, 0 = false
+
+
+//Handle each argument individually
+int index;
+for (index = 0; index < sizeof(arguments); index++)
+{
+  //stop if there are no more arguments
+  if (arguments[index] == "empty") { break; }
+
+  //This should grab the current 'string' and place it into an array of characters?
+  char* currentArgument = arguments[index];
+
+  char* currentCharacater; // first copy the pointer to not change the original
+
+ //check if first character is not b
+ if (*currentArgument != 'b')
+ {
+   printf("no\n");
+   printf("initial check\n" );
+ }
+ else {
+   //iterate through each character in order to check the Criteria.
+   //Update: as we are iterating, we will be checking the previous character. This
+   //will allow us to maintain the order of events.
+   for (currentCharacater = currentArgument; *currentCharacater != '\0'; currentCharacater++)
+   {
+
+      //Update the letterBCounter
+      if (*currentCharacater == 'b')
+      {
+        letterBCounter++;
+      }
+      //Update the colon character
+      else if (*currentCharacater == ':')
+      {
+
+        //we know that b should be done at this point. Check it's truthfulness.
+        if (letterBCounter%2 != 0) //If odd
+        {
+          printf("no\n");
+          printf("1\n");
+          should_continue = 0;
+          break;
+        }
+
+        colonCounter++;
+      }
+      //update sequence X TODO: verify that this works?
+      else if (*currentCharacater <= 0 && *currentCharacater >= 9 && sequence == 0) //This means it's a digit
+      {
+
+        //We know that the colon character should be done. Check it's truthfulness.
+        if (colonCounter != 0)
+        {
+          printf("no\n");
+          printf("2\n");
+          should_continue = 0;
+          break;
+        }
+
+        sequenceXCounter++;
+        sequenceX += *currentCharacater; //TODO: verify This works
+      }
+      else if (*currentCharacater == 'q')
+      {
+        sequence = 1;
+
+        if (sequenceXCounter > 3)
+        {
+          printf("no\n");
+          printf("3\n");
+          should_continue = 0;
+          break;
+        }
+        else if (sequenceXCounter < 1)
+        {
+          printf("no\n");
+          printf("4\n");
+          should_continue = 0;
+          break;
+        }
+
+        letterQCounter++;
+      }
+      else if (*currentCharacater == '=') { //ASCII chart
+
+        //check equals
+        if (letterQCounter > 7)
+        {
+          printf("no\n");
+          printf("6\n");
+          should_continue = 0;
+          break;
+        }
+        else if (letterQCounter < 3)
+        {
+          printf("no\n");
+          printf("7\n");
+          should_continue = 0;
+          break;
+        }
+
+        equalsCounter++;
+      }
+      else if (*currentCharacater <= 0 && *currentCharacater >= 9 && sequence == 1) //checking sequence y
+      {
+        if (equalsCounter!= 1)
+        {
+          printf("no\n");
+          printf("8\n");
+          should_continue = 0;
+          break;
+        }
+
+        sequenceY += *currentCharacater;
+      }
+      else if (*currentCharacater >= 65 && *currentCharacater <= 90) //uppercase
+      {
+          //now we can check the sequences
+          //iterate through sequence x, but only checking odds
+
+          char* sequenceXCopy = sequenceX;
+          char* currentDigit_x; // first copy the pointer to not change the original
+
+          char* newSequenceX = "";
+
+          for (currentDigit_x = sequenceXCopy; *currentDigit_x != '\0'; currentCharacater++)
+          {
+            if (currentDigit % 2 == 0) //only touch the even ones
+            {
+              newSequenceX += *currentDigit_x;
+            }
+          }
+
+
+          //then, we compare
+          if (strcmp(newSequenceX,sequenceY) != 0) //if they're not a match
+          {
+            printf("no\n");
+            printf("9\n");
+            should_continue = 0;
+            break;
+          }
+
+
+          upperCaseCount++;
+
+      }
+      else {
+        //this means a bad character was in the string
+        printf("no\n");
+        printf("Bad character\n");
+        should_continue = 0;
+        break;
+      }
+   }
+
+
+   if (should_continue != 0)
+   {
+     if (upperCaseCount%2 == 0) //If even
+     {
+       printf("no\n");
+       printf("7\n");
+     }
+     //Since it has passed all of these tests, we know it's matching so we
+     //print yes!
+     else {
+       printf("yes\n");
+
+       printf("stats:\n");
+       printf("letterBRepCounter %d\n", letterBRepCounter);
+       printf("underscoreCounter %d\n", underscoreCounter);
+       printf("letterTCounter %d\n", letterTCounter);
+       printf("characterEqualsCounter %d\n", characterEqualsCounter);
+       printf("upperCaseCount %d\n", upperCaseCount);
+     }
+   }
+ }
+
+
+
+
    printf("Flag B: \n");
 }
 
